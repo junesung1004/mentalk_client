013447f6f2a434ad313d30fed268e534661c3003
junesung1004 <junecoding1004@gmail.com>
2024-12-26 11:37:47 +0900

    배포 에러

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,19 +1,15 @@
-"use client"
+"use client";
 
 import UserType from "@/components/SelectUserType";
-import styles from "./signup.module.scss"
-import { Form } from "react-router-dom";
 
 const SignUp: React.FC = () => {
-
-    return (
-        <main>
-            <form>
-                <UserType/>
-            </form>
-        </main>
-    );
-}
-
+  return (
+    <main>
+      <form>
+        <UserType />
+      </form>
+    </main>
+  );
+};
 
 export default SignUp;
