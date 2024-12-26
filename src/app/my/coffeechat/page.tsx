7367fbdb6696cec77846b95ce495a78ae6aef3f8
junesung1004 <junecoding1004@gmail.com>
2024-12-26 11:40:38 +0900

    배포 에러

--- src/app/my/coffeechat/page.tsx
+++ src/app/my/coffeechat/page.tsx
@@ -1,134 +1,136 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
-import styles from "./coffeechat.module.scss"
+import styles from "./coffeechat.module.scss";
 import axios from "axios";
-import Coffeechat from "@/components/Coffeechat";
 import { useUserContext } from "@/context/UserContext";
 import CustomButton from "@/components/CustomButton";
 import { useRouter } from "next/navigation";
 
-const CoffeeChatPage : React.FC =  () => {
+const CoffeeChatPage: React.FC = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const { user } = useUserContext();
 
-    const {user} = useUserContext();
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const [coffeechatList, setCoffeechatList] = useState<any[]>([]);
+  const newCoffeechatList: any[] = [];
 
-    const API_URL = process.env.NEXT_PUBLIC_API_URL;
-    const [coffeechatList, setCoffeechatList] = useState<any[]>([]);
-    const newCoffeechatList : any[] = [];
+  useEffect(() => {
+    if (user?.type === "Mentor") {
+      axios
+        .get(`${API_URL}/coffeechat/mentor/${user?.id}`)
+        .then(async (result) => {
+          await result.data.data.map((el: any) => {
+            const newCoffeechat = {
+              coffeechatId: el.coffeechat.coffeechat_id,
+              introduceId: el.coffeechat.coffeechat_introduce_id,
+              wanted: el.coffeechat.coffeechat_coffee_wanted,
+              status: el.coffeechat.coffeechat_status,
+              meetingDate: el.coffeechat.coffeechat_meeting_date,
+              mentorNickname: el.mentor.mentor_nickname,
+              menteeNickname: el.mentee.mentee_nickname,
+            };
 
-    
-    useEffect(() => {
+            newCoffeechatList.push(newCoffeechat);
+          });
 
-        if(user?.type === "Mentor"){
-            axios.get(`${API_URL}/coffeechat/mentor/${user?.id}`).then(async (result) => {
-                await result.data.data.map((el : any) => {
-                    const newCoffeechat = {
-                        coffeechatId : el.coffeechat.coffeechat_id,
-                        introduceId : el.coffeechat.coffeechat_introduce_id,
-                        wanted : el.coffeechat.coffeechat_coffee_wanted,
-                        status : el.coffeechat.coffeechat_status,
-                        meetingDate : el.coffeechat.coffeechat_meeting_date,
-                        mentorNickname : el.mentor.mentor_nickname,
-                        menteeNickname : el.mentee.mentee_nickname,
-                    }
-    
-                    newCoffeechatList.push(newCoffeechat);
-                })
-    
-                setCoffeechatList(newCoffeechatList);
-    
-                console.log("newCoffeechatList");
-                console.log(newCoffeechatList);
-            }).catch((error) => {
-                console.log(error);
-            })
-        }else if(user?.type === "Mentee"){
-            axios.get(`${API_URL}/coffeechat/mentee/${user?.id}`).then(async (result) => {
-                await result.data.data.map((el : any) => {
-                    const newCoffeechat = {
-                        coffeechatId : el.coffeechat.coffeechat_id,
-                        introduceId : el.coffeechat.coffeechat_introduce_id,
-                        wanted : el.coffeechat.coffeechat_coffee_wanted,
-                        status : el.coffeechat.coffeechat_status,
-                        meetingDate : el.coffeechat.coffeechat_meeting_date,
-                        mentorNickname : el.mentor.mentor_nickname,
-                        menteeNickname : el.mentee.mentee_nickname,
-                    }
-    
-                    newCoffeechatList.push(newCoffeechat);
-                })
-    
-                setCoffeechatList(newCoffeechatList);
-            }).catch((error) => {
-                console.log(error);
-            })
-        }
-        
-       
-    }, [])
+          setCoffeechatList(newCoffeechatList);
 
-    return (
-        <>
-            <div className={styles.wrap}>
-                <div className={styles.titleContainer}>
-                    <div className={styles.title}>내 커피챗 목록</div>
-                </div>
-                <div className={styles.coffeechatContainer}>
-                    <table className={styles.tempContainer}>
-                        <thead>
-                            <tr>
-                                <th>소개글</th>
-                                <th>멘티</th>
-                                <th>날짜</th>
-                                <th>상태</th>
-                                <th>채팅</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {coffeechatList.map((coffeechat) => (
-                            <tr key={coffeechat.coffeechat_id}>
-                                <td>
-                                    <p className={styles.titleContainer}
-                                    >막연하게 느껴지는 서비스 기획, 핵심은 “이것"입니다{coffeechat.title}</p>
-                                </td> 
-                                <td>{coffeechat.menteeNickname}</td>
-                                <td>{coffeechat.meetingDate}</td>
-                                <td>
-                                    <div className={`${styles.status} 
+          console.log("newCoffeechatList");
+          console.log(newCoffeechatList);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else if (user?.type === "Mentee") {
+      axios
+        .get(`${API_URL}/coffeechat/mentee/${user?.id}`)
+        .then(async (result) => {
+          await result.data.data.map((el: any) => {
+            const newCoffeechat = {
+              coffeechatId: el.coffeechat.coffeechat_id,
+              introduceId: el.coffeechat.coffeechat_introduce_id,
+              wanted: el.coffeechat.coffeechat_coffee_wanted,
+              status: el.coffeechat.coffeechat_status,
+              meetingDate: el.coffeechat.coffeechat_meeting_date,
+              mentorNickname: el.mentor.mentor_nickname,
+              menteeNickname: el.mentee.mentee_nickname,
+            };
+
+            newCoffeechatList.push(newCoffeechat);
+          });
+
+          setCoffeechatList(newCoffeechatList);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
+
+  return (
+    <>
+      <div className={styles.wrap}>
+        <div className={styles.titleContainer}>
+          <div className={styles.title}>내 커피챗 목록</div>
+        </div>
+        <div className={styles.coffeechatContainer}>
+          <table className={styles.tempContainer}>
+            <thead>
+              <tr>
+                <th>소개글</th>
+                <th>멘티</th>
+                <th>날짜</th>
+                <th>상태</th>
+                <th>채팅</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coffeechatList.map((coffeechat) => (
+                <tr key={coffeechat.coffeechat_id}>
+                  <td>
+                    <p className={styles.titleContainer}>막연하게 느껴지는 서비스 기획, 핵심은 “이것"입니다{coffeechat.title}</p>
+                  </td>
+                  <td>{coffeechat.menteeNickname}</td>
+                  <td>{coffeechat.meetingDate}</td>
+                  <td>
+                    <div
+                      className={`${styles.status} 
                                         ${coffeechat.status === "신청" ? styles.register : ""}
                                         ${coffeechat.status === "진행중" ? styles.ongoing : ""}
                                         ${coffeechat.status === "취소" ? styles.cancel : ""}
                                         ${coffeechat.status === "완료" ? styles.done : ""}
-                                        `
-                                    }>
-                                            <p className={`${styles.statusColor}
+                                        `}
+                    >
+                      <p
+                        className={`${styles.statusColor}
                                                 ${coffeechat.status === "신청" ? styles.register : ""}
                                                 ${coffeechat.status === "진행중" ? styles.ongoing : ""}
                                                 ${coffeechat.status === "취소" ? styles.cancel : ""}
                                                 ${coffeechat.status === "완료" ? styles.done : ""}
                                             
-                                            `}></p>
-                                            {coffeechat.status}
-                                    </div>
-                                
-                                </td> 
-                                <td>
-                                    <CustomButton content="입장" onClick={() => {router.push(`/my/chatting/${coffeechat.coffeechatId}`)}}/>
-                                </td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                
-                </div>
-
-            </div>
-           
-        </>
-    )
-}
-
+                                            `}
+                      ></p>
+                      {coffeechat.status}
+                    </div>
+                  </td>
+                  <td>
+                    <CustomButton
+                      content="입장"
+                      onClick={() => {
+                        router.push(`/my/chatting/${coffeechat.coffeechatId}`);
+                      }}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default CoffeeChatPage;
--- src/app/my/favorite/page.tsx
+++ src/app/my/favorite/page.tsx
@@ -1,49 +1,48 @@
-"use client"
+"use client";
 
 import MentorProfile from "@/components/MentorProfile";
 import styles from "./favorite.module.scss";
-import axios from "axios";
+
 import { useUserContext } from "@/context/UserContext";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import useMyFavorite from "@/hook/useMyFavorite";
 
+export default async function Favorite() {
+  const router = useRouter();
 
-export default async function Favorite () {
+  const [favoriteList, setFavoriteList] = useState<Mentor[] | null>();
+  const favoriteMentorData = useMyFavorite();
 
-    const router = useRouter();
+  const { user } = useUserContext();
 
-    const [favoriteList, setFavoriteList ] = useState<Mentor[] | null>(); 
-    const favoriteMentorData = useMyFavorite();
-
-    const { user } = useUserContext();
-
-    useEffect(() => {
-        if(favoriteMentorData) {
-            setFavoriteList(favoriteMentorData);
-        }
-    }, [])
-
-    // 쿠키에서 불러온 값이 멘토라면 튕겨냄
-    if(user?.type === "Mentor"){
-        router.push("/my")
+  useEffect(() => {
+    if (favoriteMentorData) {
+      setFavoriteList(favoriteMentorData);
     }
+  }, []);
 
-    return (
-        <main>
-            <div className={styles.favoriteContainer}>
-                {
-                    favoriteList?.map((favorite : Mentor, index : number) => {
-                        return <MentorProfile
-                        key={index}
-                        nickname={favorite.nickname} 
-                        position={favorite.position}
-                        company={favorite.company}
-                        career={favorite.career}
-                        profileImg={favorite.profileImg} />
-                    })
-                }
-            </div>
-        </main>
-    )
+  // 쿠키에서 불러온 값이 멘토라면 튕겨냄
+  if (user?.type === "Mentor") {
+    router.push("/my");
+  }
+
+  return (
+    <main>
+      <div className={styles.favoriteContainer}>
+        {favoriteList?.map((favorite: Mentor, index: number) => {
+          return (
+            <MentorProfile
+              key={index}
+              nickname={favorite.nickname}
+              position={favorite.position}
+              company={favorite.company}
+              career={favorite.career}
+              profileImg={favorite.profileImg}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
 }
